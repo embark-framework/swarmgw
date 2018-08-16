ced4d82c2d515cb69543d66dede1300d3adce9b7
emizzle <ericmastro@status.im>
2018-08-16 20:16:39 +1000

    Refactor to class and add `putDirectory`
    Refactored the module to export a class, which can now be invoked using `new SwarmGW()`
    
    Added a `putDirectory` function that walks a directory, finds all files recursively, and uploads them to swarm expecting a manifest hash in return.

--- index.js
+++ index.js
@@ -1,55 +1,110 @@
-const request = require('request')
+const request = require('request');
+const fs = require('fs');
+const klaw = require('klaw');
+const through2 = require('through2');
 
-function isValidHash (hash) {
-  return /^[0-9a-f]{64}$/.test(hash)
-}
+class SwarmGW {
 
-function getFile (gateway, url, cb) {
-  request(gateway + '/' + url, function (error, response, body) {
-    if (error) {
-      cb(error)
-    } else if (response.statusCode !== 200) {
-      cb(body)
+  constructor(opts){
+    this.opts = opts || {};
+    if (this.opts.gateway) {
+      this.gateway = opts.gateway
+    } else if (this.opts.mode === 'http') {
+      this.gateway = 'http://swarm-gateways.net'
     } else {
-      cb(null, body)
+      this.gateway = 'https://swarm-gateways.net'
     }
-  })
-}
+  }
 
-function putFile (gateway, content, cb) {
-  request({
-    method: 'POST',
-    uri: gateway + '/bzz-raw:/',
-    body: content
-  }, function (error, response, body) {
+  static _isValidHash (hash) {
+    return /^[0-9a-f]{64}$/.test(hash)
+  }
+
+
+  _getDirectoryTreeReadable(directory, cb){
+    let readables = {}; // files, directories, symlinks, etc
+    let errors = [];
+
+    const excludeDirFilter = through2.obj(function (item, enc, next) {
+      if (!item.stats.isDirectory()) this.push(item)
+      next()
+    })
+
+    klaw(directory)
+    .pipe(excludeDirFilter)
+    .on('data', (item) => {
+      readables[item.path] = fs.createReadStream(item.path); 
+    })
+    .on('error', (error, item) => {
+      errors.push(`Error walking directory '${directory}', on item ${item.path}, error: ${error.message}`);
+    })
+    .on('end', () =>  cb(errors, readables)); // => [ ... array of files]
+  }
+
+  _hashResponse(error, response, body, cb){
     if (error) {
       cb(error)
     } else if (response.statusCode !== 200) {
       cb(body)
-    } else if (!isValidHash(body)) {
+    } else if (!SwarmGW._isValidHash(body)) {
       cb('Invalid hash')
     } else {
       cb(null, body)
     }
-  })
-}
+  }
 
-module.exports = function (opts) {
-  opts = opts || {}
-  var gateway
-  if (opts.gateway) {
-    gateway = opts.gateway
-  } else if (opts.mode === 'http') {
-    gateway = 'http://swarm-gateways.net'
-  } else {
-    gateway = 'https://swarm-gateways.net'
+  get(url, cb) {
+    request(this.gateway + '/' + url, function (error, response, body) {
+      if (error) {
+        cb(error)
+      } else if (response.statusCode !== 200) {
+        cb(body)
+      } else {
+        cb(null, body)
+      }
+    })
   }
-  return {
-    get: function (url, cb) {
-      return getFile(gateway, url, cb)
-    },
-    put: function (content, cb) {
-      return putFile(gateway, content, cb)
-    }
+
+  putFile (content, cb) {
+    request({
+      method: 'POST',
+      uri: this.gateway + '/bzz-raw:/',
+      body: content
+    }, (error, response, body) => this._hashResponse(error, response, body, cb))
+  }
+
+  putDirectory (path, cb) {
+    this._getDirectoryTreeReadable(path, (errors, readables) => {
+      const hasReadables = Boolean(Object.keys(readables).length);
+      if(errors.length && !hasReadables){
+        return cb(errors.join('\n'));
+      }
+      if(errors.length){
+        console.trace(errors.join('\n'));
+      }
+      if(hasReadables){
+        return request({
+          method: 'POST',
+          uri: this.gateway + '/bzz:/',
+          formData: readables
+        }, (error, response, body) => this._hashResponse(error, response, body, cb));
+      }
+      cb('No files to upload');
+    });    
+  }
+
+
+  isAvailable(cb) {
+      const testFile = "test";
+      const testHash = "6de1faa7d29b1931b4ba3d44befcf7a5e43e947cd0bf2db154172bac5ecac3a6";
+      try {
+        this.putFile(testFile, (err, hash) => {
+          if(err) return cb(err);
+          cb(null, hash === testHash);
+        });
+      } catch(e) {
+        cb(e);
+      }
   }
 }
+module.exports = SwarmGW;
